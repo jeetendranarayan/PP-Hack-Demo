9a1663d0afe53cf8edff26742c45c765419c7020
Tiwari, Jeetendra <jetiwari@paypal.com>
2019-12-08 15:56:09 +0530

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey : 'key_c825e30cff784cf983aba7de0b5311a3',
+  apiToken : 'tok_5c4e0a364f54448dbeee333f18d6fdc0',
+  contentLanguage: 'en-US',
+  chosenVoicePrintPhrase: 'Khane ko deo re',
+  dataBaseURL:  ''
+}
+
+module.exports = config;
