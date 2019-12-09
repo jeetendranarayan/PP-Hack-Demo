a77264d24ec6997662566f72d5b09aa8d8c4ba94
jeetendranarayan <jeetendranarayan@gmail.com>
2019-12-09 13:02:51 +0530

    fix.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   apiKey : 'key_c825e30cff784cf983aba7de0b5311a3',
   apiToken : 'tok_5c4e0a364f54448dbeee333f18d6fdc0',
   contentLanguage: 'en-US',
-  chosenVoicePrintPhrase: 'Eventually winter will turn into spring'
+  chosenVoicePrintPhrase: 'Hello I am very happy today'
 }
 
 module.exports = config;
