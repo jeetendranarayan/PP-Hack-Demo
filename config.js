b890bdf2a267c0d81283339e1cbe2f93ce65420e
jeetendranarayan <jeetendranarayan@gmail.com>
2019-12-09 18:32:02 +0530

    config.

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
-  apiKey : 'key_c825e30cff784cf983aba7de0b5311a3',
-  apiToken : 'tok_5c4e0a364f54448dbeee333f18d6fdc0',
-  //apiKey : 'key_e414ea03c64547d4be09266d05a8f7de',
-  //apiToken : 'tok_76dcc68d8bd94b9593bd8f26a12eddc3',
+  //apiKey : 'key_c825e30cff784cf983aba7de0b5311a3',
+  //apiToken : 'tok_5c4e0a364f54448dbeee333f18d6fdc0',
+  apiKey : 'key_e414ea03c64547d4be09266d05a8f7de',
+  apiToken : 'tok_76dcc68d8bd94b9593bd8f26a12eddc3',
   contentLanguage: 'en-US',
-  chosenVoicePrintPhrase: 'at paypal my voice is my password'
+  chosenVoicePrintPhrase: 'Never forget tomorrow is a new day'
 }
 
 module.exports = config;
