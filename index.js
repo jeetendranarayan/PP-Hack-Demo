f4bf06bd9741260247e004a39d436410465c6172
jeetendranarayan <jeetendranarayan@gmail.com>
2019-12-10 00:35:18 +0530

    done

--- index.js
+++ index.js
@@ -274,15 +274,18 @@ const processVerification = async (req, res) => {
         var authHeader = "ACfc4270a8dcd1908b732ce2c86e90e548:22c07aa43a723a413fa2b02757ed7983";
         
         var auth = "Basic " + new Buffer(authHeader).toString("base64");
-        var jsonBody = {'From' : '+919591601428', 'To': '+19896420652'};
+        //var jsonBody = {'From' : '+919591601428', 'To': '+19896420652'};
         var options = {
             method: 'POST',
             uri: 'https://studio.twilio.com/v1/Flows/FW489f1d26d21e9c3ab611ee89b0ae5520/Executions',
             headers : {
-              'Authorization' : auth
+              'Authorization' : auth,
+              'Content-Type': 'application/x-www-form-urlencoded'
             },
-            body: jsonBody,
-            json: true
+            form: {
+              From : '+919591601428',
+              To: '+19896420652'
+            }
         };
 
         rp(options)
