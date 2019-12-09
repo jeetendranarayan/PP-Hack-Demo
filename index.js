16478aac192e5d9ca54f274e90b1ad06133369c4
jeetendranarayan <jeetendranarayan@gmail.com>
2019-12-10 01:40:25 +0530

    test

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const callerUserId = async (phone) => {
 
 const incomingCall = async (req, res) => {
   const twiml = new VoiceResponse();
-  console.log(req);
+  console.log(req.body);
   const phone = removeSpecialChars(req.body.From);
   const userId = await callerUserId(phone);
 
